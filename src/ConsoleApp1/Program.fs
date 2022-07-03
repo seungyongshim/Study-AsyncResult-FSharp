dde972315495e46f2cd61d6129a54267b4ba6ee9
SeungYongShim <syshim@me.com>
2022-07-03 18:46:23 +0900

    .

--- src/ConsoleApp1/Program.fs
+++ src/ConsoleApp1/Program.fs
@@ -3,18 +3,19 @@ type Effect<'a> =
     | Result of 'a
 
 let handle effect =
-    let rec loop log = function
+    let rec loop log =
+        function
         | Log (str, cont) ->
             let log' = str :: log
             loop log' (cont ())
         | Result res -> res, log
+
     let result, log = loop [] effect
     result, log |> List.rev
 
-let rec bind f = function
-    | Log (str , cont) ->
-        Log (str, fun () ->
-            cont () |> bind f)
+let rec bind f =
+    function
+    | Log (str, cont) -> Log(str, (fun () -> cont () |> bind f))
     | Result res -> f res
 
 type EffectBuilder() =
@@ -23,7 +24,7 @@ type EffectBuilder() =
 
 let effect = EffectBuilder()
 
-let log str = Log (str, fun () -> Result ())
+let log str = Log(str, (fun () -> Result()))
 let logf fmt = Printf.ksprintf log fmt
 
 let hypotenuse a b =
@@ -37,6 +38,6 @@ let hypotenuse a b =
 
 let q = hypotenuse 1. 2.
 
-let c, logs  = q |> handle
+let c, logs = q |> handle
 
 printf "%A" logs
